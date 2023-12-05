b169f391a7126329c734f6f45029abe756592cc2
ecee <you@example.com>
2023-12-05 14:26:12 +0300

    Update

--- lib/song.py
+++ lib/song.py
@@ -1,5 +1,6 @@
 import sqlite3
 
+
 CONN = sqlite3.connect('music.db')
 CURSOR = CONN.cursor()
 
@@ -49,3 +50,31 @@ class Song:
         return song
 
     # new code goes here!
+
+    @classmethod
+    def new_from_db(cls, row):
+        song = cls(row[1], row[2])
+        song.id = row[0]
+
+    @classmethod
+    def all(cls):
+        sql = """
+            SELECT *
+            FROM songs
+        """
+
+        all = CURSOR.execute(sql).fetchall()
+
+    
+    @classmethod
+    def find_by_name(cls, name):
+        sql = """
+            SELECT *
+            FROM songs
+            WHERE name = ?
+            LIMIT 1
+        """
+
+        song = CURSOR.execute(sql, (name,)).fetchone()
+
+        return cls.new_from_db(song)
